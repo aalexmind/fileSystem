5a0bf3702a5364ad8847e07294b6b156e2c5be0c
ialex <aalexmind@google.com>
2021-08-04 14:47:59 +0300

    small refactor

--- src/com/ialex/Main.java
+++ src/com/ialex/Main.java
@@ -7,8 +7,27 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner stdin = new Scanner(new BufferedInputStream(System.in));
+        FileSystem fs = new FileSystem();
         while (stdin.hasNext()) {
-            System.out.println(stdin.next());
+            String input = stdin.next();
+            String[] split = input.split(" ");
+            if (split.length <= 1) {
+                System.out.println("Try again");
+            } else {
+                switch (split[0]) {
+                    case "ls":
+                        System.out.println(fs.listFiles(split[1]));
+                        break;
+                    case "rm":
+                        fs.removeFile(split[1]);
+                        break;
+                    case "touch":
+                        fs.addFile(split[1]);
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
